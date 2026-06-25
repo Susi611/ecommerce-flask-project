cd382ca7dec1a7a7eeb26fb1afeb1d503bc4765f
Susi Sushanthini <susisushanthini@gmail.com>
2026-06-25 11:00:58 +0530

    Updated Product Page

--- test_db.py
+++ test_db.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
 
-client = MongoClient(
-    "mongodb+srv://susisushanthini_db_user:Susi@12345@cluster0.ttnchb1.mongodb.net/EcommerceDB?retryWrites=true&w=majority"
-)
+uri = "mongodb+srv://susisushanthini_db_user:Susi12345@cluster0.ttnchb1.mongodb.net/EcommerceDB?retryWrites=true&w=majority"
 
-db = client["EcommerceDB"]
-
-print(db.list_collection_names())
+try:
+    client = MongoClient(uri)
+    print(client.list_database_names())
+    print("Connected Successfully")
+except Exception as e:
+    print("Connection Failed")
+    print(e)
